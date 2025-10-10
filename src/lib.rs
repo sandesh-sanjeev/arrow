27c4bb2da3729046106cd8c0a0273498a9d0a00c
Sandesh Sanjeev <sandesh.achr@gmail.com>
2025-10-09 19:18:35 -0700

    Reset crate to new

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,1 @@
 //! Arrow
-
-// TODO: Remove after initial implementation.
-#![allow(dead_code)]
-
-pub(crate) mod avec;
-pub(crate) mod lock;
-
-pub use avec::AtomicVec;
-
-/// Synchronization primitives swapped out during concurrency tests.
-mod sync {
-    pub use std::hint::spin_loop;
-    pub use std::sync::*;
-}
